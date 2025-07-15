1e65576345e484bfdbcc87d51ab718932b876135
ula <ulahyle@gmail.com>
2025-07-15 15:15:04 +0330

    add Store and Customer models

--- store/models.py
+++ store/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, blank=True)
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    owner = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='stores')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
