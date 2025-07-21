aec393cd19c58801ddd63946f53a1bf3548a89ef
ula <ulahyle@gmail.com>
2025-07-21 20:49:32 +0330

    Refactored project structure for better modularity: added new apps to organize related models!

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'store'
+    'market',
+    'accounts',
+    'order',
+    'cart',
+    'reviews',
+    'core'
 ]
 
 MIDDLEWARE = [
