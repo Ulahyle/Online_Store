bc03ceb5616132cc14c0765532bd150193872e79
ula <ulahyle@gmail.com>
2025-07-21 21:02:10 +0330

    setup Redis

--- config/settings.py
+++ config/settings.py
@@ -152,3 +152,13 @@ SIMPLE_JWT = {
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",)
 }
+
+CACHES = {
+    "default":{
+        "BACKEND": "django-redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS":{
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        }
+    }
+}
