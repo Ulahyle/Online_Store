64e70e39a26b75884150a98e8e5c83683e740e16
ula <ulahyle@gmail.com>
2025-07-21 20:44:01 +0330

    delete out of use app: store

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('store/', include('store.urls')),
+    # path('store/', include('store.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
