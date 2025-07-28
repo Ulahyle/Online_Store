e15736b3ea512ba1a16bfc04ea460ac5261f5aab
ula <ulahyle@gmail.com>
2025-07-28 11:14:15 +0330

    add Customer model!

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Customer(AbstractUser):
+    phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    email_verified = models.BooleanField(default=False)
+    is_vendor = models.BooleanField(default=False)
