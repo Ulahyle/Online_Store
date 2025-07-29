5e529686121a19b113972d9f4622e5c023233b70
ula <ulahyle@gmail.com>
2025-07-29 10:09:38 +0330

    fix redis cache backend settings

--- config/settings.py
+++ config/settings.py
@@ -161,7 +161,7 @@ REDIS_DB = 0
 
 CACHES = {
     "default":{
-        "BACKEND": "django-redis.cache.RedisCache",
+        "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS":{
             "CLIENT_CLASS": "django_redis.client.DefaultClient"
