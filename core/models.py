456dc1afa0cdd289549ec4b2db1c89fb4df191de
ula <ulahyle@gmail.com>
2025-08-30 20:21:13 +0330

    feat: add AbstractBaseModel with soft delete and timestamps

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class AbstractBaseModel(models.Model):
+    is_deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False): # soft delete
+        self.is_deleted = True
+        self.save(update_fields=["is_deleted", "updated_at"])
