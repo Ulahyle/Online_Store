948f6ee39b339ad2fae655b62ed5a11fd03f86c7
ula <ulahyle@gmail.com>
2025-08-30 20:25:37 +0330

    feat: make Customer inherit from AbstractBaseModel for timestamps and softdelete

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from core.models import AbstractBaseModel
 
 
-class Customer(AbstractUser):
+class Customer(AbstractUser, AbstractBaseModel):
     phone_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
     email_verified = models.BooleanField(default=False)
     is_vendor = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username or self.email or str(self.id)
