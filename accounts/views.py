f76412351c66019fa79abb813182a900bb6ebd3e
ula <ulahyle@gmail.com>
2025-09-05 11:44:33 +0330

    fix(views): name error in otp verification by importing q

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 import random
+from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
